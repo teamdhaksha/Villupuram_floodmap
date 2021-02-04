132339ac5fe959b59bf7a9e615a9e9b0ff090b12
teamdhaksha <63536148+teamdhaksha@users.noreply.github.com>
2021-02-04 12:09:14 +0530

    Add files via upload

---
+++ data/Towns_7.js
@@ -0,0 +1,14 @@
+var json_Towns_7 = {
+"type": "FeatureCollection",
+"name": "Towns_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "NAME": "Ulundurpet" }, "geometry": { "type": "Point", "coordinates": [ 79.289135847347637, 11.692729519604759, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Villupuram" }, "geometry": { "type": "Point", "coordinates": [ 79.491789683146649, 11.943460229387529, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Chinnasalem" }, "geometry": { "type": "Point", "coordinates": [ 78.875784583413321, 11.63339765673334, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Kallakurichi" }, "geometry": { "type": "Point", "coordinates": [ 78.957899767599258, 11.73807623629231, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Tindivanam" }, "geometry": { "type": "Point", "coordinates": [ 79.647752493049495, 12.23898177224946, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Gingee" }, "geometry": { "type": "Point", "coordinates": [ 79.418315580173541, 12.25532157637984, -999999.0 ] } },
+{ "type": "Feature", "properties": { "NAME": "Thirukoilure" }, "geometry": { "type": "Point", "coordinates": [ 79.204490559178268, 11.96660306363597, -999999.0 ] } }
+]
+}
